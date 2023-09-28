6549a36dc6f1aa6d4dc5a65f34998b3cf8c6e833
Kirill Skorikov <92673983+skorikovkr@users.noreply.github.com>
2023-09-28 19:36:58 +0500

    first commit

---
+++ Models/Basket.php
@@ -0,0 +1,46 @@
+<?php
+
+class Basket{
+    private $id;
+    private $user_id;
+    private $products;
+
+    public function __construct() {
+        // ...
+    }
+    
+    public function get_id()
+    {
+        return $this->id;
+    }
+    
+    public function set_id($id)
+    {
+        $this->id = $id;
+    }
+    
+    public function get_user_id()
+    {
+        return $this->user_id;
+    }
+    
+    public function set_user_id($user_id)
+    {
+        $this->user_id = $user_id;
+    }
+    
+    public function add_product($product, $quantity) 
+    { 
+        if (!isset($this->products[$product->get_id()])) {
+            $this->products[$product->get_id()] = 0;
+        }
+        $this->products[$product->get_id()] += $quantity;
+    }
+
+    public function remove_product($product, $quantity) 
+    { 
+        if (isset($this->products[$product->get_id()])) {
+            $this->products[$product->get_id()] -= $quantity;
+        }
+    }
+}
---
+++ Models/User.php
@@ -0,0 +1,86 @@
+<?php
+
+class User{
+    private $id;
+    private $first_name;
+    private $last_name;
+    private $patronim;
+    private $address;
+    private $email;
+    private $password;
+    private $profile_picture;
+
+    public function __construct(){
+        // ...
+    }
+    
+    public function getId()
+    {
+        return $this->id;
+    }
+    
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+    
+    public function get_first_name()
+    {
+        return $this->first_name;
+    }
+    
+    public function set_first_name($first_name)
+    {
+        $this->first_name = $first_name;
+    }
+    
+    public function get_last_name()
+    {
+        return $this->last_name;
+    }
+
+    public function set_last_name($last_name)
+    {
+        $this->last_name = $last_name;
+    }
+    
+    public function get_patronim()
+    {
+        return $this->patronim;
+    }
+    
+    public function set__patronim($patronim)
+    {
+        $this->patronim = $patronim;
+    }
+
+    public function get_email()
+    {
+        return $this->email;
+    }
+    
+    public function set_email($email)
+    {
+        $this->email = $email;
+    }
+    
+    public function get_password()
+    {
+        return $this->password;
+    }
+
+    public function set_password($password)
+    {
+        $this->password = $password;
+    }
+
+    public function get_profile_picture()
+    {
+        return $this->profile_picture;
+    }
+    
+    public function set_profile_picture($profile_picture)
+    {
+        $this->profile_picture = $profile_picture;
+    }
+}

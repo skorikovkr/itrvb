34794d168e6d873e9f35eabb62c923cc5396e867
Skorikov Kirill <skorikovkr@gmail.com>
2023-11-17 16:12:30 +0500

    Used faker

--- index.php
+++ index.php
@@ -4,6 +4,7 @@ require_once __DIR__ . '/vendor/autoload.php';
 use Root\Skorikov\Models\Post;
 use Root\Skorikov\Models\User;
 use Root\Skorikov\Models\Comment;
+use Faker\Factory;
 
 // Реализуйте автозагрузчик классов согласно следующим правилам:
 // 1. Разделитель пространства имён преобразуется в разделитель папок: / для Linux и MacOS или \ для Windows. 
@@ -17,8 +18,10 @@ use Root\Skorikov\Models\Comment;
 //     }
 // });
 
-$user = new User(0, "Kirill", "Skorikov");
-$user2 = new User(1, "User2-firstName", "User2-lastName");
+$faker = Factory::create();
+
+$user = new User(0, $faker->firstName(), $faker->lastName());
+$user2 = new User(1, $faker->firstName(), $faker->lastName());
 $post = new Post(0, $user, "Post Title", "Post text");
 $comment = new Comment(0, $user2, $post, "Comment text");
 

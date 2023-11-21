f5d15c2732ff52fb2fde51bdc43c36aa3038639f
Skorikov Kirill <skorikovkr@gmail.com>
2023-11-21 21:54:22 +0500

    Phpunit installation

---
+++ tests/Test.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Root\Skorikov\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+final class Test extends TestCase
+{
+    public function testTrue(): void
+    {
+        $this->assertTrue(true);
+    }
+}
